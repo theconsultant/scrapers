4b2669cad5c0b1b2fca15337512b63116d3c4b6d
alexcreek <acreek83@yahoo.com>
2014-09-01 19:42:37 -0400

    add exceptions

--- table_grabber.py
+++ table_grabber.py
@@ -21,52 +21,34 @@ def parse_rows(rows):
     """ Get data from rows """
     results = []
     for row in rows:
-        table_headers = row.find_all('th')
-        if table_headers:
-            results.append([headers.get_text() for headers in table_headers])
-
-        table_data = row.find_all('td')
+        table_data = row.find_all(['th', 'td'])
         if table_data:
-            results.append([data.get_text() for data in table_data])
+            results.append([data.get_text().strip() for data in table_data])
     return results
 
 
 def main():
     # Get arguments
     args = parse_arguments()
-    if args.url:
-        url = args.url
+    url = args.url
 
-    # Make soup
     try:
         resp = urlopen(url)
     except URLError as e:
-        print 'An error occured fetching %s \n %s' % (url, e.reason)   
-        return 1
+        raise ValueError('Error fetching - %s' %  e.reason)   
     soup = BeautifulSoup(resp.read())
     
-    # Get table
     try:
         table = soup.find('table')
-    except AttributeError as e:
-        print 'No tables found, exiting'
-        return 1
-
-    # Get rows
-    try:
         rows = table.find_all('tr')
     except AttributeError as e:
-        print 'No table rows found, exiting'
-        return 1
+        raise ValueError('No valid table found')
 
-    # Get data
     table_data = parse_rows(rows)
 
-    # Print data
     for i in table_data:
        print '\t'.join(i)
 
 
 if __name__ == '__main__':
-    status = main()
-    sys.exit(status)
+    main()
