32a307581ed662be064b24608b3268d870e86a8c
alexcreek <acreek83@yahoo.com>
2014-09-02 22:48:04 -0400

    add _get_subdomains, _save_results, correct URL syntax checking

--- spider.py
+++ spider.py
@@ -3,6 +3,7 @@ import bs4
 import re
 import urlparse
 import argparse
+import pdb
 
 class Spider():
 
@@ -13,54 +14,72 @@ class Spider():
     def get_links(self, url):
         """ Extract link urls from <a> tags """
 
-        # Test if URL has valid syntax
-        if not 'http://' in url[:7] or 'https://' in url[:8]:
-            return None
-        
         self.visited_urls.append(url)
+
         try:
             resp = requests.get(url)
         except:
             return None
 
+
         soup = bs4.BeautifulSoup(resp.content)
-        links = [tag.get('href') for tag in soup.find_all('a')]
+        links = [tag.get('href').strip() for tag in soup.find_all('a') if tag.get('href')]
 
         # ToDo Fixup relative links to absolute
 
         for link in links:
-            if link:
-                if not 'mailto:' in link[:7]:
-                    self.stored_urls.append(link)
+            if not 'mailto:' in link[:7]:
+                self.stored_urls.append(link)
         return links
 
     def crawl(self, url):
         """ Recursivly extract links starting with supplied url  """
         
         # Test if URL has valid syntax
-        if not 'http://' in url[:7] or 'https://' in url[:8]:
-            raise ValueError('%s is not a valid URL' % url)
+        if not 'http://' in url[:7]:
+            if not 'https://' in url[:8]: 
+                raise ValueError('%s is not a valid URL' % url)
 
         # Parse URL
-        url_parts = urlparse.urlparse(url)
+        self.domain_name = urlparse.urlparse(url)[1]
 
         # Call get_links() to seed stored_urls with initial url
-        print "Beginning at %s" % url
+        print "[*] STARTING AT %s" % url
         for link in self.get_links(url):
             print 'Found %s' % link
         
-        print 'Beginning crawling of stored_urls'    
+        print '[*] BEGINNING CRAWLING OF STORED URLS'
         for stored_url in self.stored_urls:
-            if not stored_url:
-                continue
-            if not re.search(url_parts.netloc, stored_url):
+            if not re.search(self.domain_name, stored_url):
                 continue
             if '%20%20%20' in stored_url:
                 continue
             if stored_url in self.visited_urls:
                 continue
             print stored_url 
-            self.get_links(stored_url)
+            self.get_links(stored_url) 
+        self._save_results()
+
+    def _get_subdomains(self):
+        """ Parse visited_urls and retrieve subdomains """
+
+        self.subdomains = set()
+
+        for visited_url in self.visited_urls:
+            fqdn = urlparse.urlparse(visited_url)[1]
+            if self.domain_name in fqdn:
+                if len(fqdn.split('.')) > 2:
+                    self.subdomains.add(fqdn.split('.')[0])
+
+    def _get_directories(self):
+        """ Parse visited_urls and create a directory structure """
+
+     
+    def _save_results(self):
+        """ Clean lists and write stored/visited/subdomains to disk """
+
+        # Creates self.subdomains
+        self._get_subdomains()
 
         # Remove dups, become sets
         self.stored_urls = set(self.stored_urls)
@@ -69,22 +88,31 @@ class Spider():
         # Sort, saved back to lists
         self.stored_urls = sorted(self.stored_urls)
         self.visited_urls = sorted(self.visited_urls)
+        self.subdomains = sorted(self.subdomains)
 
         print 'Stored URLS: %i' % len(self.stored_urls)
         print 'Visited URLS: %i' % len(self.visited_urls)
+        print 'Subdomains: %i' % len(self.subdomains)
 
         # ToDo Create a resutls dir for each site crawled
-        filename_visited = 'visited-urls-' + url_parts.netloc
-        filename_stored = 'stored-urls-' + url_parts.netloc
+        filename_visited = 'visited-urls-' + self.domain_name
+        filename_stored = 'stored-urls-' + self.domain_name
+        filename_subdomain = 'subdomains-' + self.domain_name
 
         with open(filename_visited, 'w') as f_visited:
             for visited in self.visited_urls:
-                f_visited.write(visited + '\n')
+                if visited:
+                    f_visited.write(visited + '\n')
 
         with open(filename_stored, 'w') as f_stored:
             for stored in self.stored_urls:
                 if stored:
                     f_stored.write(stored + '\n')
+
+        with open(filename_subdomain, 'w') as f_subdomain:
+            for subdomain in self.subdomains:
+                if subdomain:
+                    f_subdomain.write(subdomain + '\n')
 
 
 def parse_arguments():
